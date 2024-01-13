0b3659ae477510d308314787c2993ba6abf36046
chrzbrk <156067510+chrzbrk@users.noreply.github.com>
2024-01-13 15:14:21 +0100

    Minor polishing

--- script.js
+++ script.js
@@ -18,26 +18,25 @@ function positionEyes() {
     eye2.style.top = (picHeight * eye2Y) + 'px';
 }
 
+// Position Eyes first time loading the page
+positionEyes();
 // EventListener Eyeposition by resizing
 window.addEventListener('resize', positionEyes);
 
-// Position Eyes first time loading the page
-positionEyes();
 
-
-// --------------- PUPILS ------------------------------
+// --------------- MOOO-VE PUPILS ------------------------------
 
 function movepupils(x, y) {
     const eyes = document.querySelectorAll('.eye');
 
     eyes.forEach(eye => {
         const rect = eye.getBoundingClientRect();
-        const eyeX = rect.left + rect.width / 1;
-        const eyeY = rect.top + rect.height / 1;
+        const eyeX = rect.left + rect.width / 2;
+        const eyeY = rect.top + rect.height / 2;
         const deltaX = x - eyeX;
         const deltaY = y - eyeY;
         const degree= Math.atan2(deltaY, deltaX);
-        const maxDistance = rect.width / 4; // Maximaler Versatz der Pupille
+        const maxDistance = rect.width / 4;
         const distance = Math.min(maxDistance, Math.hypot(deltaX, deltaY));
 
         const pupil = eye.querySelector('.pupil');
@@ -46,6 +45,7 @@ function movepupils(x, y) {
 }
 
 document.addEventListener('mousemove', function(event) {movepupils(event.clientX, event.clientY);});
+
 
 // --------------- SPEECH BUBBLE ------------------------------
 
@@ -56,29 +56,30 @@ function positionSpeechBubble() {
     if (pic && speechBubble) {
         const rect = pic.getBoundingClientRect();
         
-        const bubbleX = rect.width * 0.17; // Z.B. 50% der Bildbreite
-        const bubbleY = rect.height * 0.37; // Z.B. 30% der Bildhöhe
+        const bubbleX = rect.width * 0.17; 
+        const bubbleY = rect.height * 0.37;
 
         speechBubble.style.left = bubbleX + 'px';
         speechBubble.style.top = bubbleY + 'px';
     }
 }
 
-// Event-Listener für das Laden der Seite und Änderungen der Fenstergröße
+// EventListener Position Bubble by resizing
 window.addEventListener('load', positionSpeechBubble);
 window.addEventListener('resize', positionSpeechBubble);
 
 
-
+// Show Bubble and add paragraphs
 document.getElementById('button_speech').addEventListener('click', function() {
     var text = document.getElementById('input_speech').value;
     var speechBubble = document.getElementById('speech_bubble');
     var newParagraph = document.createElement('p');
     newParagraph.textContent = text;
     speechBubble.appendChild(newParagraph);
-    speechBubble.style.display = 'block'; // Sprechblase anzeigen
+    speechBubble.style.display = 'block';
 });
 
+// Reset Bubble
 document.getElementById('button_reset').addEventListener('click', function() {
     var speechBubble = document.getElementById('speech_bubble');
     speechBubble.innerHTML = ''; // Entfernt alle Paragraphen
